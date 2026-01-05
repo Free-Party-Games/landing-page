c7bf948d3c7793ce26fec64b9d46e3eb401e5500
Rajesh Nathani <rajeshnathani@Rajeshs-MacBook-Air.local>
2026-01-05 22:13:13 +0530

    improved seo

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  headers: async () => [
+  {
+    source: '/sitemap.xml',
+    headers: [{ key: 'Content-Type', value: 'application/xml' }],
+  },
+]
   /* config options here */
 };
 
